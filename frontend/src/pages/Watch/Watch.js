811f8def05ebf7a5da30497800df46ce4cd6ee3d
AhsanAntal <ahsan.jatttt@gmail.com>
2022-09-12 12:30:36 -0700

    Presntation

--- frontend/src/pages/Watch/Watch.js
+++ frontend/src/pages/Watch/Watch.js
@@ -5,9 +5,6 @@ import "./watch.scss";
 import { useLocation } from "react-router-dom";
 
 export default function Watch() {
-  const location = useLocation();
-  console.log(location);
-
   const activeMovie = location.movie;
 
   return (
